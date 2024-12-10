ad2f8867f81cb7a51dd2ac825fd7bde4c7951e85
qaz4042 <404286846@qq.com>
2024-12-10 20:33:57 +0800

    feat: test github action

---
+++ src/main/java/com/example/common/service/CommonService.java
@@ -0,0 +1,10 @@
+package com.example.common.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommonService {
+    public String getMessage() {
+        return "Hello from CommonLib!";
+    }
+}
